e57b2663d3dafc96642156c4e0c187389578e1f7
dwissaaj <dwisaji150@gmail.com>
2025-02-16 14:40:43 +0700

    fix member id bug

--- app/(member)/member/[memberid]/[name]/@profile/page.tsx
+++ app/(member)/member/[memberid]/[name]/@profile/page.tsx
@@ -1,33 +1,6 @@
 import ProfileCard from "@/app/components/member/ProfileCard";
 import { GetProfile } from "@/app/utils/member/profile/getProfile";
-export async function generateMetadata({
-  params,
-}: {
-  params: Promise<{ memberid: string; name: string }>;
-}) {
-  const id = (await params).memberid;
-  const memberName = (await params).name;
-  const member = await GetProfile(id);
-  const { name, group, generation, birthplace, debut, graduate, height } =
-    member;
-  const title = `${name} ${group.charAt(0).toUpperCase() + group.slice(1).toLowerCase()}`;
-  const description = `${name} is a ${generation}-generation member of ${group}. Born in ${birthplace}, she debuted on ${new Date(
-    debut,
-  ).toLocaleDateString()} and is currently ${graduate}. Height: ${height} cm.`;
 
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-      type: "profile",
-      url: `https://sakamichi.cloud/member/${id}/${memberName
-        .toLowerCase()
-        .replace(/\s+/g, "-")}`,
-    },
-  };
-}
 
 export default async function Page({
   params,
@@ -36,6 +9,7 @@ export default async function Page({
 }) {
   const { memberid } = await params;
   const memberData = await GetProfile(memberid);
+  console.log("GET REQ GET PROFILE")
   return (
     <ProfileCard
       profilePic={memberData.profilePic}
--- app/(member)/member/[memberid]/[name]/page.tsx
+++ app/(member)/member/[memberid]/[name]/page.tsx
@@ -1,3 +1,34 @@
+import { GetProfile } from "@/app/utils/member/profile/getProfile";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ memberid: string; name: string }>;
+}) {
+  const id = (await params).memberid;
+  const memberName = (await params).name;
+  const member = await GetProfile(id);
+  const { name, group, generation, birthplace, debut, graduate, height } =
+    member;
+  const title = `${name} ${group.charAt(0).toUpperCase() + group.slice(1).toLowerCase()}`;
+  const description = `${name} is a ${generation}-generation member of ${group}. Born in ${birthplace}, she debuted on ${new Date(
+    debut,
+  ).toLocaleDateString()} and is currently ${graduate}. Height: ${height} cm.`;
+  
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: "profile",
+      url: `https://sakamichi.cloud/member/${id}/${memberName
+        .toLowerCase()
+        .replace(/\s+/g, "-")}`,
+    },
+  };
+}
+
 export default async function Page() {
   return (
     <div className="flex flex-col items-center justify-center">
